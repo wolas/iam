5976764ee5786f79bebf5c1c3cd28ecb1ecdf6f4
Jake <jcpenche@gmail.com>
2013-08-07 16:03:38 +0200

    geocoder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+  before_filter :get_location
   
+  private
+  def get_location
+    @location = Geocoder.search(request.ip)
+  end
 end
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,14 +1,7 @@
 class StaticController < ApplicationController
   
-  before_filter :get_location
-  
   def index
     
   end
   
-  private
-  def get_location
-    @location = Geocoder.search(request.ip)
-  end
-
 end
