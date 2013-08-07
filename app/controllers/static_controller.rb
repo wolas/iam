f47f93d183955dc8d0c6bc275e493df7e30c29e4
Jake <jcpenche@gmail.com>
2013-08-07 15:55:12 +0200

    geocoding example

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,14 @@
 class StaticController < ApplicationController
   
+  before_filter :get_location
+  
+  def index
+    
+  end
+  
+  private
+  def get_location
+    @location = Geocoder.search(request.ip)
+  end
 
 end
