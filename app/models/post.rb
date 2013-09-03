b4fe8c83f3ef7b62df5f7e5d2fc120d3201fc43d
Jake <jcpenche@gmail.com>
2013-09-03 20:23:31 +0200

    adding photos to blog posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
   has_many :comments
+  
+  has_one :photo, :as => :avatar
+  has_many :photos, :as => :parent
 
   def short_body num=450
     return "" if body.blank?
